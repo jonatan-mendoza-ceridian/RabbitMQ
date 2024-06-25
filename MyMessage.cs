61b84865ea5d1e0eef2aeb22b3b0e1be42e655ab
jonatan-mendoza-ceridian <123120965+jonatan-mendoza-ceridian@users.noreply.github.com>
2024-06-25 16:47:55 -0600

    Add project files.

---
+++ MyMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQTest
+{
+    public class MyMessage
+    {
+        public int id {  get; set; }
+        public string? Message {  get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQTest;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+var message = new MyMessage { 
+    id = 0,
+    Message = "Hello World!!" 
+};
+
+var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+channel.BasicPublish(exchange: string.Empty,
+                     routingKey: "hello",
+                     basicProperties: null,
+                     body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
